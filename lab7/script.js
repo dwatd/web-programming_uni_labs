bbe7ac8541d3acb3d567b5807751f596c31eb812
dwatd <semenyukkate4@gmail.com>
2024-12-01 19:01:23 +0200

    lab7 proj1

---
+++ lab7/script.js
@@ -0,0 +1,7 @@
+// proj1
+function showMe() {
+  let input = document.getElementById("show-me-input");
+  let inputValue = document.getElementById("show-me-input").value;
+  console.log(inputValue);
+  input.value = "";
+}
